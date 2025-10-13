ab0d8793080ee4c97e1deb267a9ce56f968488c9
yu <spike100@naver.com>
2025-10-13 17:29:37 +0900

    수정:add_timestamp 구현

--- posts.py
+++ posts.py
@@ -1,11 +1,18 @@
+from datetime import datetime
+
 def create_post(title, content, author):
-    post = {"title": title, "content": content, "author": author}
-    return post
+    return {"title": title, "content": content, "author": author}
 
 def get_post_summary(post):
     return f"제목: {post['title']}, 작성자: {post['author']}"
 
-#테스트
-post = create_post("공지사항", "서비스 점검안내", "admin")
-print(post)
-print(get_post_summary(post))
+def add_timestamp(post):
+    # 새 딕셔너리 생성
+    return {**post, "created_at": datetime.now()}
+    
+
+# 테스트
+post = create_post("공지사항", "서비스 점검 안내", "admin")
+post_with_time = add_timestamp(post)
+print(post_with_time)
+print(get_post_summary(post_with_time))
